34b77d4b6e6fb6f8a236029a8eec8718e6be8f02
Gabriel Farias <b-fariasgabriel@outlook.com>
2026-04-22 20:19:32 -0300

    fix: update serverBase URL in configuration file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Example: window.SHOWDO_CONFIG = { serverBase: 'https://show-do-miau.onrender.com' }
 
 window.SHOWDO_CONFIG = window.SHOWDO_CONFIG || {};
-window.SHOWDO_CONFIG.serverBase = "https://quizia.farigab.com";
+window.SHOWDO_CONFIG.serverBase = "https://api-quizia.farigab.com";
 
 // NOTE: This file is safe to commit (it only exposes a public URL). If you
 // prefer not to commit a production config, create `config.js` locally or
